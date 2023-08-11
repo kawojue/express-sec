a2b4c555940d7421f60804ad583e624d62aee72b
Raheem Kawojue <kawojue08@gmail.com>
2023-08-11 13:40:17 +0100

    [add] verification

---
+++ middlewares/verification.ts
@@ -0,0 +1,57 @@
+import jwt from 'jsonwebtoken'
+import prisma from '../prisma'
+import compareObjects from '../utils/compareObj'
+import { NextFunction, Request, Response } from 'express'
+const expressAsyncHandler = require('express-async-handler')
+
+
+const verify = expressAsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization
+    const deviceId = {
+        ipAddress: req.ip,
+        userAgent: req.headers['user-agent']
+    }
+    if (!authHeader || !authHeader?.startsWith('Bearer')) {
+        res.status(401).json({
+            success: false,
+            msg: "Access denied"
+        })
+        return
+    }
+
+    const token: string = authHeader.split(' ')[1]
+
+    jwt.verify(
+        token,
+        process.env.JWT_SECRET!,
+        async (err: any, decoded: any) => {
+            if (err) {
+                res.status(403).json({
+                    success: false,
+                    msg: "Access denied"
+                })
+                return
+            }
+
+            const account = await prisma.users.findFirst({
+                where: { token }
+            })
+
+            const authDeviceId = account?.deviceId
+            const decodedDeviceId = decoded.deviceId
+
+            const result: boolean = compareObjects([authDeviceId, decodedDeviceId, deviceId])
+
+            if (!result) {
+                res.status(403).json({
+                    success: false,
+                    msg: "Unauthorized IP or Device"
+                })
+                return
+            }
+            next()
+        }
+    )
+})
+
+export default verify
