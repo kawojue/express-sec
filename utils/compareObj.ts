7d39c445ae1af590851cb35a365f74578ee80946
Raheem Kawojue <kawojue08@gmail.com>
2023-08-11 13:40:09 +0100

    [add] verification

---
+++ utils/compareObj.ts
@@ -0,0 +1,21 @@
+function compareObjects(objects: any[]) {
+    if (objects.length === 0) {
+        return false
+    }
+
+    const referenceObject: any = objects[0]
+    for (let i = 1; i < objects.length; i++) {
+        const currentObject: any = objects[i]
+
+        if (
+            currentObject.ip !== referenceObject.ip ||
+            currentObject.userAgent !== referenceObject.userAgent
+        ) {
+            return false
+        }
+    }
+
+    return true
+}
+
+export default compareObjects
